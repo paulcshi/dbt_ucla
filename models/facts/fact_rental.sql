cc30f90261e752cd521d220be62aeba4d61d48dd
Paul Shih <paulcshi@gmail.com>
2022-02-23 06:51:46 +0000

    rental update

--- models/facts/fact_rental.sql
+++ models/facts/fact_rental.sql
@@ -3,13 +3,13 @@
 
 select 
     stg_rental.rental_id, 
-    dim_customer.customer_key,
+    dim_customer_type_two.customer_key,
     dim_staff.staff_key,
     1 as rental_quantity
 from 
     {{ref('stg_rental')}}
-    left join {{ref('dim_customer')}}
-        on stg_rental.customer_id = dim_customer.customer_id
+    left join {{ref('dim_customer_type_two')}}
+        on stg_rental.customer_id = dim_customer_type_two.customer_id
     left join {{ref('dim_staff')}}
         on stg_rental.staff_id = dim_staff.staff_id
 
