ea0ed12b6a3140124b2656f83b8019a159c4d720
Paul Shih <paulcshi@gmail.com>
2022-03-01 19:15:22 +0000

    creating tod and date

--- models/facts/fact_rental.sql
+++ models/facts/fact_rental.sql
@@ -1,33 +1,50 @@
 {{ config(materialized='table') }}
 
 
-select 
-    stg_rental.rental_id, 
-    dim_customer_type_two.customer_key,
-    dim_staff.staff_key,
-    1 as rental_quantity
-from 
-    {{ref('stg_rental')}}
-    left join {{ref('dim_customer_type_two')}}
-        on stg_rental.customer_id = dim_customer_type_two.customer_id
-    left join {{ref('dim_staff')}}
-        on stg_rental.staff_id = dim_staff.staff_id
-
-
-
--- {{ config(materialized='table') }}
-
 -- select 
---     stg_rental.rental_id,
---     dim_customer.customer_key,
+--     stg_rental.rental_id, 
+--     dim_customer_type_two.customer_key,
 --     dim_staff.staff_key,
 --     1 as rental_quantity
--- from
---     {{ ref('stg_rental') }}
---     left join {{ ref('dim_customer') }}
---         on stg_rental.customer_id = dim_customer.customer_id
---     left join {{ ref('dim_staff') }}
+-- from 
+--     {{ref('stg_rental')}}
+--     left join {{ref('dim_customer_type_two')}}
+--         on stg_rental.customer_id = dim_customer_type_two.customer_id
+--     left join {{ref('dim_staff')}}
 --         on stg_rental.staff_id = dim_staff.staff_id
+
+
+SELECT d.DATE_KEY as rental_date_key,
+tod.TIMEOFDAY_KEY as rental_timeofday_key,
+//HOUR(r.rental_date),
+//r.rental_date,
+r.rental_id,
+c.CUSTOMER_KEY,
+f.FILM_KEY,
+sd.staff_key,
+sl.STORE_LOCATION_KEY,
+1 as rental_quantity,
+p.amount
+-- select count(*)
+FROM  
+    {{ref('stg_rental')}} r
+     JOIN {{ref('dim_customerxxxxx')}} d 
+        ON to_number(to_varchar(to_date(r.rental_date),'YYYYMMDD')) = d.DATE_KEY
+     JOIN {{ref('dim_customerxxxxx')}} tod 
+        ON HOUR(r.rental_date) = tod.HROFDAY AND MINUTE(r.rental_date) = tod.MINOFDAY
+     JOIN {{ref('dim_customer')}} c 
+        ON r.customer_id = c.customer_id
+     JOIN {{ref('stg_inventoery')}} i 
+        ON r.inventory_id = i.inventory_id
+     JOIN {{ref('dim_store')}} sl 
+        ON i.store_id = sl.STORE_ID
+     JOIN {{ref('dim_film')}} f 
+        ON  f.film_id = i.film_id
+     JOIN {{ref('dim_staff')}} sd 
+        ON r.staff_id = sd.STAFF_ID
+     JOIN  {{ref('stg_payment')}}" p 
+        ON r.rental_id = p.rental_id
+        WHERE to_date(r.rental_date) < '2020-01-01' 
 
 
 
