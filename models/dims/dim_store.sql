92d843d907d4040aca6077a93191190d4db9cff9
Paul Shih <paulcshi@gmail.com>
2022-03-01 18:57:33 +0000

    dim_store created

--- models/dims/dim_store.sql
+++ models/dims/dim_store.sql
@@ -1,18 +1,19 @@
 {{ config(materialized='table') }}
 
-select 
-    stg_store.store_id,
+SELECT 
+    {{ dbt_utils.surrogate_key(['s.store_id']) }} as STORE_LOCATION_KEY ,
+    s.store_id,
+	a.postal_code,
+	c.city,
+	a.district,
+	co.country,
+	a.phone
+FROM {{ref('stg_store')}} s
+JOIN {{ref('stg_address')}} a 
+    ON s.address_id = a.address_id
+JOIN {{ref('stg_city')}} c 
+    ON a.city_id = c.city_id
+JOIN {{ref('stg_country')}} co 
+    ON c.country_id = co.country_id
 
-    stg_address.district,
-    stg_address.address,
-    stg_address.phone,
 
-    stg_staff.last_name
-
-
-from 
-    {{ref('stg_store')}}
-left join {{ref('stg_address')}}
-    on stg_store.address_id = stg_address.address_id
-left join {{ref('stg_staff')}}
-    on stg_store.manager_staff_id = stg_staff.staff_id
